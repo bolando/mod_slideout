7b796ef39dacd2c34f7b786eded3db385bc74b46
bolando <biuro@solmedia.pl>
2017-06-24 20:29:11 +0200

    aktualizacja

--- tmpl/default.php
+++ tmpl/default.php
@@ -2,25 +2,38 @@
 // No direct access
 defined('_JEXEC') or die; 
 $doc->addStyleSheet('modules/mod_slideout/css/mod_slideout.css');
-$doc->addScript('modules/mod_slideout/js/jquery.tabSlideOut.v1.3.js');?>
+$doc->addScript('modules/mod_slideout/js/jquery.tabSlideOut.v1.3.js');
+
+if ($params->get('obrazek') == 'custom') {
+	$obrazek = $params->get('custom-image');
+	$wymiary = getimagesize($obrazek);
+	$height = $wymiary[0];
+	$width = $wymiary[1];
+} else {
+	$obrazek = 'modules/mod_slideout/images/contact_tab.gif';
+	$height = '122';
+	$width = '40';
+	}
+getimagesize 
+?>
 
 <script>
 	jQuery(function(){
-		jQuery('.demo').tabSlideOut({
+		jQuery('.slideout').tabSlideOut({
 		tabHandle:'.handle',  //class of the element that will be your tab
-		pathToTabImage:'/images/contact_tab.gif',//path to the image for the tab (optionaly can be set using css)
-		imageHeight:'122px',   //height of tab image
-		imageWidth:'40px',   //width of tab image   
-		tabLocation: 'left',   //side of screen where tab lives, top, right, bottom, or left
+		pathToTabImage:<?php echo '\'' .$obrazek . '\''; ?>,//path to the image for the tab (optionaly can be set using css)
+		imageHeight:<?php echo '\'' . $height . 'px\''; ?>,   //height of tab image
+		imageWidth:<?php echo '\'' . $width . 'px\''; ?>,   //width of tab image   
+		tabLocation: <?php echo '\'' . $params->get('pozycja') . '\''; ?>,   //side of screen where tab lives, top, right, bottom, or left
 		speed: 300,   //speed of animation
 		action:'click',//options: 'click' or 'hover', action to trigger animation
-		topPos:'200px',//position from the top
+		topPos:<?php echo '\'' . $params->get('odleglosc') . 'px\''; ?>,//position from the top
 		fixedPosition:true    //options: true makes it stick(fixed position) on scroll
 		});
 	});
 </script>
 
 <div class="slideout">
-<a class="handle" href="#"></a>
-<jdoc:include type="modules" name="slideout" style="xhtml" />
+<a class="handle" href="#">tekst</a>
+<?php echo $params->get('tresc'); ?>
 </div>
